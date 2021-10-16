6a97e691988e1394da483fb93ba520d4a2dd0a55
_isha_30 <choudhary.isha30@gmail.com>
2021-10-16 22:46:10 +0530

    Function calling function

--- RectArea/src/com/geekyscript/Triangle.java
+++ RectArea/src/com/geekyscript/Triangle.java
@@ -3,8 +3,12 @@ package com.geekyscript;
 public class Triangle {
     int a, b , c;
     public double getArea(int a, int b, int c){
-        float s = (a+b+c)/2;
+        float s = getPeri(a, b, c)/2;
         double area = Math.pow(s*(s-a)*(s-b)*(s-c), 0.5);
         return area;
+    }
+    public int getPeri(int a, int b, int c){
+        int p = a+b+c;
+        return p;
     }
 }
